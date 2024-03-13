31b6920c886ace5810eeb2f7abc0c50f16bebed1
Wala Thabet <36531358+WalaThabet@users.noreply.github.com>
2024-03-13 14:43:59 +0100

    Add pagination

--- src/CharacterList.jsx
+++ src/CharacterList.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import Character from "./Character";
 import { useQuery, gql } from "@apollo/client";
+import { Button, IconButton } from "@material-tailwind/react";
 
 const CharacterList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const [nameFilter, setNameFilter] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-
   const GET_CHARACTERS = gql`
-    query GetCharacters($name: String) {
-      characters(filter: { name: $name }) {
+    query GetCharacters($page: Int) {
+      characters(page: $page) {
+        info {
+          count
+          pages
+          next
+          prev
+        }
         results {
           id
           name
@@ -21,19 +28,27 @@ const CharacterList = () => {
     }
   `;
   const { loading, error, data } = useQuery(GET_CHARACTERS, {
-    variables: { name: searchTerm },
+    variables: { page: currentPage },
   });
+
+  const goToNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   const handleSearch = () => {
     setSearchTerm(nameFilter);
   };
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :</p>;
-
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Rick and Morty Characters</h1>
+      {error && <p>Error: {error.message}</p>}
       <br></br>
       <input
         className="flex-grow rounded-l-md border border-gray-200 px-4 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
@@ -52,9 +67,28 @@ const CharacterList = () => {
       <br></br>
       <br></br>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {data.characters.results.map((character) => (
-          <Character key={character.id} character={character} />
-        ))}
+        {data &&
+          data.characters.results.map((character) => (
+            <Character key={character.id} character={character} />
+          ))}
+      </div>
+      <br></br>
+
+      <div className="flex flex-row items-center gap-2">
+        <button
+          onClick={goToPreviousPage}
+          className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={currentPage <= 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={goToNextPage}
+          className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={!data || !data.characters.info.next}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
